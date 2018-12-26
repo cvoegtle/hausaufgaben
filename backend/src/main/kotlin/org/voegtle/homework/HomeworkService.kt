18e3b7e0e561b421347c1882ecafa1736f98f600
christian@voegtle.org <christian@voegtle.org>
2018-12-26 12:57:05 +0100

    Erlaubte Origins vorgeben

--- backend/src/main/kotlin/org/voegtle/homework/HomeworkService.kt
+++ backend/src/main/kotlin/org/voegtle/homework/HomeworkService.kt
@@ -13,7 +13,7 @@ import java.util.logging.Level
 import java.util.logging.Logger
 
 @RestController class HomeworkService {
-  @GetMapping("/list") @CrossOrigin(origins = ["*"]) fun list(): List<Hausaufgabe> {
+  @GetMapping("/list") @CrossOrigin(origins = ["https://www.tom-schimmeck.de", "https://www.tom-schimmeck.de", "http://localhost:4200"]) fun list(): List<Hausaufgabe> {
     return ObjectifyService.ofy().load().type(Hausaufgabe::class.java).filter("datum >=", sevenDaysBefore()).order("-datum").list()
   }
 
